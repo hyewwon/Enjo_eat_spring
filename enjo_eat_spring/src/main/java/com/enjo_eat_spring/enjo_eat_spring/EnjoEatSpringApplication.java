9b2c018ee9287c0278d840e0313c4380f57a651a
hyewwon <rlagp424@gmail.com>
2024-01-04 10:23:37 +0900

    init commit

---
+++ enjo_eat_spring/src/main/java/com/enjo_eat_spring/enjo_eat_spring/EnjoEatSpringApplication.java
@@ -0,0 +1,13 @@
+package com.enjo_eat_spring.enjo_eat_spring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EnjoEatSpringApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(EnjoEatSpringApplication.class, args);
+    }
+
+}
---
+++ enjo_eat_spring/src/test/java/com/enjo_eat_spring/enjo_eat_spring/EnjoEatSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.enjo_eat_spring.enjo_eat_spring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EnjoEatSpringApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
