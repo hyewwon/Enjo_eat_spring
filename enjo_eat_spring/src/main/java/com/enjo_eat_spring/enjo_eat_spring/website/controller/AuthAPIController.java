24667ba64e97df966c4817c87a9b6f6f27a80ed5
hyewwon <rlagp424@gmail.com>
2024-01-17 17:57:54 +0900

    chore : check_userdata

--- enjo_eat_spring/src/main/java/com/enjo_eat_spring/enjo_eat_spring/website/controller/AuthAPIController.java
+++ enjo_eat_spring/src/main/java/com/enjo_eat_spring/enjo_eat_spring/website/controller/AuthAPIController.java
@@ -1,14 +1,16 @@
 package com.enjo_eat_spring.enjo_eat_spring.website.controller;
 
-import com.enjo_eat_spring.enjo_eat_spring.common.response.BasicResponseDTO;
-import com.enjo_eat_spring.enjo_eat_spring.common.response.ResponseEnum;
 import com.enjo_eat_spring.enjo_eat_spring.data.dto.UserDTO;
 import com.enjo_eat_spring.enjo_eat_spring.website.service.AuthService;
 import lombok.RequiredArgsConstructor;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
@@ -21,15 +23,24 @@ public class AuthAPIController {
         this.authService = authService;
     }
     @PostMapping("/join")
-    public BasicResponseDTO<Object> JoinUser(@RequestBody UserDTO.CreateRequestDTO createRequestDTO){
+    public ResponseEntity<Map<String, Boolean>> JoinUser(@RequestBody UserDTO.CreateRequestDTO createRequestDTO){
         authService.joinUser(createRequestDTO);
-        return BasicResponseDTO.getSuccessMessage(true, ResponseEnum.OK);
+        Map<String,Boolean> map = new HashMap<>();
+        map.put("success", true);
+
+        return ResponseEntity.ok().body(
+                map
+        );
     }
 
+    @ResponseBody
     @PostMapping("/check-userData")
-    public BasicResponseDTO<Object> CheckUserId(@RequestParam("type") String type, @RequestParam("data") String userData){
+    public ResponseEntity<Map<String, String>> CheckUserId(@RequestBody Map<String, String> param){
         log.info("====================in=========================");
-        boolean result = authService.checkUserData(userData, type);
-        return BasicResponseDTO.getSuccessMessage(result, ResponseEnum.OK);
+//        boolean result = authService.checkUserData(userData, type);
+        Map<String,Boolean> map = new HashMap<>();
+        map.put("success", true);
+
+        return ResponseEntity.ok(param);
     }
 }
